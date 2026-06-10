877248c951780245abb3eaffb292740a86b4eb28
Leonardo Silva <leonardovgds21@icloud.com>
2026-06-10 13:29:53 -0300

    adicionei a opção do usuário digitar os nomes via prompt, ao invés de abrir o arquivo config.py

--- config.py
+++ config.py
@@ -3,11 +3,3 @@ PASTA_ORIGEM = r"C:\Users\Leonardo\OneDrive - GAP SSMA\Leonardo Vinicius\4 Autom
 
 # Cole aqui o caminho da pasta de destino
 PASTA_DESTINO = r"C:\Users\Leonardo\OneDrive - GAP SSMA\Leonardo Vinicius\4 Automacao\pasta_destino\holerites"
-
-# Lista de nomes dos colaboradores que você quer filtrar
-COLABORADORES = [
-    "adauto pereira caetano",
-    "ademir biscalchini",
-    "leonardo vinicius galvao da silva",
-    "adriana tavares"
-]
