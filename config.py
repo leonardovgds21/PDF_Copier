65d7b3fd37a574726b01d9c98aab02f8020fb4a8
Leonardo Silva <leonardovgds21@icloud.com>
2026-06-10 13:45:05 -0300

    eliminei o arquivo config.py, pois já não é mais necessário

--- config.py
+++
@@ -1,5 +0,0 @@
-# Cole aqui o caminho da pasta de origem
-PASTA_ORIGEM = r"C:\Users\Leonardo\OneDrive - GAP SSMA\Leonardo Vinicius\4 Automacao\pasta_origem"
-
-# Cole aqui o caminho da pasta de destino
-PASTA_DESTINO = r"C:\Users\Leonardo\OneDrive - GAP SSMA\Leonardo Vinicius\4 Automacao\pasta_destino\holerites"
